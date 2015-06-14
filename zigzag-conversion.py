9c7f3140c7ad9e5b3884cd9b1d656d40bbf12ff6
Chang Yu-heng <mr.changyuheng@gmail.com>
2015-06-14 15:10:16 +0800

    #6 ZigZag Conversion

---
+++ zigzag-conversion.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    # @param {string} s
+    # @param {integer} numRows
+    # @return {string}
+    def convert(self, s, numRows):
+        n = len(s)
+        res = ''
+        for i in range(numRows):
+            if i >= n:
+                break
+            if i == 0 or i == numRows - 1:
+                res += s[i::(numRows-1)*2 if (numRows-1)*2 > 0 else 1]
+            else:
+                k = i
+                down = True
+                while k < n:
+                    res += s[k]
+                    if down:
+                        k += (numRows - 1 - i) * 2
+                    else:
+                        k += (i - 0) * 2
+                    down = not down
+        return res
+
+print(Solution().convert("PAYPALISHIRING", 4))
